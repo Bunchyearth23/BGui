e22cbe394d0428868721dbc7b403c75de6df7a4c
Bunchyearth23 <adecafmeyer@gmail.com>
2025-10-15 14:55:34 +0200

    init button

---
+++ src/widgets/button.rs
@@ -0,0 +1,71 @@
+use crate::globals::Globals;
+use crate::widgets::common::{DrawCommand, Drawable, Widget};
+use std::collections::HashMap;
+use std::sync::Arc;
+use std::sync::Mutex;
+use wgpu_glyph::ab_glyph::PxScale;
+use wgpu_glyph::{Extra, Section, Text};
+
+pub struct BButton {
+    text: String,
+    position: (f32, f32),
+    func: Option<Arc<Mutex<dyn FnMut(&mut Self, &mut HashMap<String, Globals>)>>>,
+}
+
+impl BButton {
+    pub fn new(text: String, pos: (f32, f32)) -> Self {
+        Self {
+            text: text,
+            position: pos,
+            func: None,
+        }
+    }
+
+    pub fn add_function<F: FnMut(&mut Self, &mut HashMap<String, Globals>) + 'static>(
+        &mut self,
+        in_func: F,
+    ) {
+        self.func = Some(Arc::new(Mutex::new(in_func)))
+    }
+
+    pub fn get_text(&self) -> String {
+        self.text.clone()
+    }
+
+    pub fn set_text(&mut self, text_in: String) {
+        self.text = text_in
+    }
+}
+
+impl Drawable for BButton {
+    fn draw_command(&self) -> Vec<super::common::DrawCommand<'_>> {
+        let mut commands = Vec::<DrawCommand>::new();
+        let section: Section<Extra> = Section {
+            screen_position: (self.position.0, self.position.1),
+            text: vec![
+                Text::new(self.text.as_str())
+                    .with_color([1.0, 1.0, 1.0, 1.0])
+                    .with_scale(PxScale { x: 32.0, y: 32.0 }),
+            ],
+            ..Default::default()
+        };
+        commands.push(DrawCommand::Text(section));
+        commands
+    }
+}
+
+impl Widget for BButton {
+    fn drawable(&self) -> Box<dyn Drawable> {
+        Box::new(Self {
+            text: self.text.clone(),
+            position: self.position,
+            func: None,
+        })
+    }
+    fn update(&mut self, global: &mut HashMap<String, Globals>) {
+        if let Some(function) = self.func.clone() {
+            let mut lock = function.lock().expect("Poisoned");
+            lock.call_mut((self, global));
+        }
+    }
+}
--- src/widgets/mod.rs
+++ src/widgets/mod.rs
@@ -1,2 +1,3 @@
+pub mod button;
 pub mod common;
 pub mod text;
