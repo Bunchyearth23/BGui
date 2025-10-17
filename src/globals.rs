618e8c59a587dcac7fa4914d61f10a0f6ee2671f
Bunchyearth23 <adecafmeyer@gmail.com>
2025-10-17 09:45:50 +0200

    Impl globals test

--- src/globals.rs
+++ src/globals.rs
@@ -1,6 +1,14 @@
+use crate::bgui::BGui;
+
 pub enum Globals {
     Empty,
     Int(i32),
     Text(String),
     Float(f32),
+    Other(Box<dyn Global>),
+}
+
+pub trait Global {
+    fn get(&self, ui: &BGui) -> Box<dyn Global>;
+    fn set(&self, id: String, ui: &mut BGui);
 }
